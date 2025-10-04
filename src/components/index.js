a9ebd87984f6135d98846804f0e75b1052d3ab2b
malikk-tahir <tahirikhlaq90@gmail.com>
2025-10-04 13:48:22 +0500

    Your message

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import Header from "./header/header";
-import Footer from "./footer/footer";
+import Header from "./Header_temp/Header";
+import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
